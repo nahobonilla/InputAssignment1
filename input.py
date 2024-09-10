6448633228da26d7fdc295f6f5cf7aa1c4a57544
Delmy Bonilla <bonildel001@warren.k12.in.us>
2024-09-10 08:24:31 -0400

    initial commit

---
+++ input.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+droids = int(input("How many droids do you want to meet"))
+wookiees = int(input("How many wookies do you want to meet"))
+print(name, "wants to meet", droids, "droids and", wookiees,"Wookiees!")
